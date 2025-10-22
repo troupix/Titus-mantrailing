c26b96eaa11666868f6080c14f62be898c4f0bd4
Troupix <maximilien.tordeux@gmail.com>
2025-10-22 16:01:02 +0200

    Fix: Standardize component folder casing to lowercase

--- src/Components/TitusLogo.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TitusLogo: React.FC<{ size?: number }> = ({ size = 24 }) => {
-  return <img src={process.env.PUBLIC_URL + '/TitusLogo.png'} alt="Titus Logo" style={{ width: `${size}px`, height: `${size}px` }} />;
-};
-
-export default TitusLogo;
